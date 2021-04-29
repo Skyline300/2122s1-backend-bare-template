6b0d8d12657dd9b52038d5635acd1345af06a3ab
Skyline300 <79264371+Skyline300@users.noreply.github.com>
2021-04-29 13:02:56 +0800

    some things

--- router.js
+++ router.js
@@ -23,5 +23,33 @@ app.get('/', function (req, res, next) {
 });
 // after: the first middleware
 app.use('/queue', queueRoute);
+// before: module.exports = app;
 
+// 404
+app.use((req, res, next) => next(new errors.UrlNotFoundError(`${req.method} ${req.originalUrl} Not Found`)));
+
+// error handler
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, next) => {
+    // Console.error for quick debugging using console
+    console.error(error); // eslint-disable-line no-console
+
+    // Extract information
+    let status = 500;
+    let code = ERROR_CODE.UNEXPECTED_ERROR;
+    let message = 'Unexpected Error!';
+    const reason = error.message;
+
+    // Special case of errors
+    if (error instanceof errors.UrlNotFoundError) {
+        status = 404;
+        code = ERROR_CODE.URL_NOT_FOUND;
+        message = `Resource not found`;
+    }
+
+    const payload = { code, error: message, reason };
+
+    // Log and respond accordingly.
+    return res.status(status).json(payload);
+});
 module.exports = app; // this should always be the last line
