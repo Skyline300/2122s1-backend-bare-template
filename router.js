f36dc657118e640d70458890aef13d6468c5700b
Skyline300 <79264371+Skyline300@users.noreply.github.com>
2021-04-29 12:05:07 +0800

    new file plus additional router code

--- router.js
+++ router.js
@@ -1,3 +1,19 @@
 const { app } = require('./app');
-
+app.get('/', function (req, res, next) {
+    return {
+        message: 'Welcome to our queue system, the following are the supported APIs',
+        apis: [
+            {
+                name: 'Enqueue',
+                endpoint: '/queue',
+                method: 'POST',
+            },
+            {
+                name: 'Dequeue',
+                endpoint: '/queue',
+                method: 'DELETE',
+            },
+        ],
+    };
+});
 module.exports = app; // this should always be the last line
