962dce950f21befb84d5607ff6257bd9a4bcf151
Skyline300 <79264371+Skyline300@users.noreply.github.com>
2021-04-29 12:53:14 +0800

    I dont know what im doing

--- router.js
+++ router.js
@@ -1,4 +1,7 @@
 const { app } = require('./app');
+// after: const { app } = require('./app');
+const queueRoute = require('./routes/queue_route');
+
 app.get('/', function (req, res, next) {
     return {
         message: 'Welcome to our queue system, the following are the supported APIs',
@@ -16,4 +19,7 @@ app.get('/', function (req, res, next) {
         ],
     };
 });
+// after: the first middleware
+app.use('/queue', queueRoute);
+
 module.exports = app; // this should always be the last line
